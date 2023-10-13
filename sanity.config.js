d024f63dc8c33dc13e38c5fc4ad92aff5709d65f
Konstantin Shilkov <konst.shilkov@gmail.com>
2023-10-13 15:20:42 +0200

    11

---
+++ sanity.config.js
@@ -0,0 +1,17 @@
+import { defineConfig} from "sanity";
+import { deskTool } from "sanity/desk"; 
+
+const config = defineConfig({
+
+projectId: "8n8yk8j9",
+dataset: "production",
+title: "next-sanity-studio",
+apiVersion: "2023-03-25",
+plugins: [deskTool(), visionTool({defaultApiVersion: apiVersion})],
+basePath : "/admin",
+
+
+
+})
+
+export default config
